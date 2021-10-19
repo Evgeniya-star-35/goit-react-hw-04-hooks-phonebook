24200f3cdbccc81a232953cd1eaae34ee274bed2
Evgeniya Bayda <baje75128@gmail.com>
2021-10-19 20:50:02 +0300

    update change

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,12 +8,22 @@ export default function Form({ onSubmit }) {
   const [number, setNumber] = useState('');
 
   const handleInputChange = e => {
-    setName(e.target.value);
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  const handlerInputNumberChange = e => {
-    setNumber(e.target.value);
-  };
+  // const handlerInputChange = e => {
+  //   setNumber(e.target.value);
+  // };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -50,7 +60,7 @@ export default function Form({ onSubmit }) {
         <input
           value={number}
           className={s.input}
-          onChange={handlerInputNumberChange}
+          onChange={handleInputChange}
           type="tel"
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
